2d339d6c97d3f8c8a38020a1b6b21062efcc09d9
JXA <Ambion_J@yahoo.com>
2017-07-24 13:19:27 -0700

    Added additional if conditions

--- Section 9 - JavaScript Basics Control Flow/script97.js
+++ Section 9 - JavaScript Basics Control Flow/script97.js
@@ -1,10 +1,25 @@
-age = prompt("How old are you?");
+var age = Number(prompt("How old are you?"));
 
-if(age < 18){
-	console.log("Sorry, you are not old enough to enter the venue");
+if(age < 0){
+	console.log("No negatives!")
 }
 else if(age < 18){
+	console.log("Sorry, you are not old enough to enter the venue");
+}
+else if(age < 21){
 	console.log("You may enter, but you cannot drink");
-} else {
+} 
+else if(age === 21){
+	console.log("Happy 21st birthday! You can also turn up!")
+}
+else {
 	console.log("You can turn up");
+}
+
+if((age % 2) !== 0){
+	console.log("Your age is odd");
+} 
+
+if(age % Math.sqrt(age) === 0) {
+	console.log("Perfect square");
 }
