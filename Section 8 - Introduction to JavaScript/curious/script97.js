475f55fb4239abb37d3eeb85392b26aec1079d77
JXA <Ambion_J@yahoo.com>
2017-07-24 13:04:07 -0700

    Begin Section 9

---
+++ Section 8 - Introduction to JavaScript/curious/script97.js
@@ -0,0 +1,10 @@
+age = prompt("How old are you?");
+
+if(age < 18){
+	console.log("Sorry, you are not old enough to enter the venue");
+}
+else if(age < 18){
+	console.log("You may enter, but you cannot drink");
+} else {
+	console.log("You can turn up");
+}
