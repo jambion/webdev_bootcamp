33ae37fb341f6d4675af762e8a0e2acc83704290
JXA <Ambion_J@yahoo.com>
2017-07-25 19:04:31 -0700

    Complete ToDo List

--- Section 11 - Basics Arrays/todoList/list.js
+++ Section 11 - Basics Arrays/todoList/list.js
@@ -1,17 +1,43 @@
 var choice = prompt("What would you like to do?");
-var myList = [];
+var todos = ["Buy New Turtle"];
 var newTodo;
+
+function createNewTodo(){
+	newTodo = prompt("Enter new todo:");
+	todos.push(newTodo);
+	console.log("New Todo Added! :D");
+}
+
+function listTodos(){
+	console.log("********");
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+	console.log("********");
+}
+
+function deleteTodo(){
+	// ask for index of todo to be deleted
+	var index = prompt("Enter index of todo to delete");
+	//delete that todo
+	//splice()
+	todos.splice(index, 1);
+	console.log("Deleted Todo");
+
+}
 
 while(choice !== "quit"){
 	if(choice === "new") {
-		newTodo = prompt("What would you like to add to the list?");
-		myList.push(newTodo);
+		createNewTodo();
 	} else if(choice === "list"){ 
-		for(var i = 0; i < myList.length; i++){
-			console.log(myList[i]);
-		}
+		listTodos();
+	} else if(choice === "delete"){
+		deleteTodo();
 	} else {
 		alert("Not a valid input. Please try again")
 	}
 	choice = prompt("What would you like to do?");
 }
+
+
+console.log("Thank you for using the app my friend :D");
