511c3144de890c0f7336308c8c1a673aa33761cb
JXA <Ambion_J@yahoo.com>
2017-07-25 17:20:58 -0700

    Comeplete Section 9

---
+++ Section 9 - JavaScript Basics Control Flow/annoyomatic/question.js
@@ -0,0 +1,12 @@
+var answer = prompt("Are we there yet?");
+
+// while(answer !== "yes" && answer !== "yeah"){
+// 	answer = prompt("Are we there yet?");
+// }
+
+// BONUS: 
+while(answer.indexOf("yes") === -1 && answer.indexOf("yeah") === -1){
+	answer = prompt("Are we there yet?");
+}
+
+alert("Yay, we finally made it!");
---
+++ Section 9 - JavaScript Basics Control Flow/forLoops/forLoops.js
@@ -0,0 +1,25 @@
+console.log("Q1: ");
+for(var i = -10; i <= 19; i++){
+	console.log(i);
+}
+
+console.log("Q2: ");
+for(var j = 10; j <= 40; j++){
+	if(j % 2 === 0){
+		console.log(j);
+	}
+}
+
+console.log("Q3: ");
+for(var k = 300; k <= 333; k++){
+	if(k % 2 !== 0){
+		console.log(k);
+	}
+}
+
+console.log("Q4: ");
+for(var l = 5; l <= 50; l++){
+	if(l % 5 === 0 && l % 3 === 0){
+		console.log(l);
+	}
+}
